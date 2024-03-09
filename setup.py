cb8109e4c54904036810fffaf01321436b22c4b9
GitHub Action <action@github.com>
2024-03-09 19:18:09 +0000

    Bump version to 0.1.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 repo_url = "https://github.com/lguibr/commitai"
 setup(
     name="commitai",
-    version="0.1.8",
+    version="0.1.9",
     author="Luis Guilherme",
     author_email="lgpelin92@gmail.com",
     packages=find_packages(),
